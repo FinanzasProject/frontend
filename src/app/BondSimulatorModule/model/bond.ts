1c1fb8387c638ac1f997c4960b23b67b4d8940c3
Alejandro Espino <espinofloresa@gmail.com>
2025-06-02 00:25:38 -0500

    fix: bond fixed

--- src/app/BondSimulatorModule/model/bond.ts
+++ src/app/BondSimulatorModule/model/bond.ts
@@ -1,15 +1,14 @@
 export class Bond {
   id!: number;
-  user_id!: number;
   name!: string;
+  issue_date!: string;
   nominal_amount!: number;
   interest_rate!: number;
-  rate_type!: string;
-  payment_frequency!: number;
-  amortization_method!: string;
+  rate_type!: 'nominal' | 'efectiva';
+  rate_period!: string;
+  payment_frequency!: string;
   term!: number;
   total_grace!: number;
   partial_grace!: number;
   initial_cost!: number;
-  issue_date!: string;
 }
